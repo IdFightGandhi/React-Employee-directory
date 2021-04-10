bd8a30fba5cf798e4ec3268be9912a9e92c9903e
William Thorne <williamcthorne@gmail.com>
2021-04-10 16:47:40 -0700

    Header renders

---
+++ employee-directory/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header"
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <Header />
+
+            </div>
+        </Router>
+    )
+}
+export default App;
