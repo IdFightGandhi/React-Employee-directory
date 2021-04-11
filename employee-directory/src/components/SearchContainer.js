bf0ea0326daa611dc986ce915abca4112fc94a12
William Thorne <williamcthorne@gmail.com>
2021-04-10 17:45:14 -0700

    making search container boilerplate then filter employees

--- employee-directory/src/components/SearchContainer.js
+++ employee-directory/src/components/SearchContainer.js
@@ -50,6 +50,55 @@ handleFormSubmit = event => {
 
 };
 
+filterEmployees = (value) => { console.log("Search result: ", this.state.search)
+    const newArray = this.state.filteredUser.filter(item => {
 
+        let values = Object.values(item)
+        .join("")
+        .toLowerCase();
+        return values.indexOf(this.state.search.toLowerCase()) !== -1;
+    });
+    console.log(newArray);
+    this.setState({ filteredUser: newArray})
+
+}
+
+render() {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <h1>Employee Directory</h1>
+                </div>
+            </div>
+            <div className="row">
+            <div className="col-lg-6">
+                <SearchForm
+                search={this.state.search}
+                handleFormSubmit={this.handleFormSubmit}
+                handleInputChange={this.handleInputChange}
+                />
+                <ResultList result={this.state.filteredUser} />
+            </div>
+            </div>
+
+            <div className="row">
+                <table className="table">
+                    <tr>
+                        <th scope="col">Employee Photo</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone Number</th>
+                        <th scope="col">D.O.B.</th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
+}
+};
 
 export default SearchContainer
+
+
