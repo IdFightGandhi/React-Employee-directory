94fd41276e80327558071efc466ddd2d08a785f1
William Thorne <williamcthorne@gmail.com>
2021-04-11 10:57:08 -0700

    trying to get search function to search

--- employee-directory/src/components/SearchContainer.js
+++ employee-directory/src/components/SearchContainer.js
@@ -8,17 +8,27 @@ class SearchContainer extends Component {
         filteredUser: [],
         search:"",
         filter:"",
-        filterBy:"lastName",
+        filteredEmployees:[],
         currentSort: "default",
-        sortField:""
+        sortDirection: this.initialSortDirection,
     };
+
+    get initialSortDirection() {
+        return{
+            name:"",
+            phone:"",
+            email: "",
+            dob:"",
+        };
+    }
 
     componentDidMount() {
         API.search()
         .then(res=>this.setState({
             result:res.data.results,
-            filteredUser: res.data.results
-        }))
+            filteredEmployees: res.data.results
+        })
+        )
         .catch(err => console.log(err));
     
 };
@@ -27,27 +37,61 @@ handleInputChange = event => {
     event.preventDefault();
     console.log(event);
     const name = event.target.name;
-    const value = event.target.value;
+    // const value = event.target.value;
     console.log("HANDLE INPUT CHANGE")
     console.log(name);
-    console.log(value);
+    // console.log(value);
     this.setState({
-        [name]: value
+        [name]: name
     });
+    this.filteredEmployees(name.toLowerCase().trim());
 };
 
 handleFormSubmit = event => {
     event.preventDefault();
     console.log(event)
-
     const name = event.target.name;
-    const value = event.target.value;
+    // const value = event.target.value;
     console.log("HANDLE FORM SUBMIT")
     console.log(name);
-    console.log(value);
+    // console.log(value);
     // this.filterEmployees();
+};
 
+sortBy =(key, primary =0, secondary = 0) => {
+    let sortedEmployees = this.state.filteredEmployees;
+    if (this.state.sortDirection[key]) {
+        this.setState({
+            filteredEmployees: sortedEmployees.reverse(),
+            sortDirection: {
+                ...this.initialSortDirection,
+                [key]: this.state.sortDirection[key] === "asc" ? "desc" : "asc",
+            },
+        });
+    }else {
+        sortedEmployees = this.state.filteredEmployees.sort((a,b) => {
+            a = a[key];
+            b = b[key];
 
+            if(primary) {
+                if (secondary && a[primary] === b[primary]) {
+                    return a[secondary].localeCompare(b[primary]);
+                }else {
+                    return a.localeCompare(b);
+                }
+            }
+            console.log(sortedEmployees)
+
+        });
+
+        this.setState({
+            filteredEmployees: sortedEmployees,
+            sortDirection: {
+                ...this.initialSortDirection,
+                [key]: "asc",
+            },
+        });
+    }
 };
 
 // filterEmployees = (value) => { console.log("Search result: ", this.state.search)
